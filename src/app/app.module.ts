fda807b930c0eba0c648c3313c4ff74a56064423
--list <isaquebatista400@gmail.com>
2025-03-29 09:09:13 -0300

    :sparkles: feat: Table componente created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableUsersComponent } from './table-users/table-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableUsersComponent
   ],
   imports: [
     BrowserModule,
